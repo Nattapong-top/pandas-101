db1a009deb0b558ba1205c893fdd64fc1c092773
Nattapong-top <37808442+Nattapong-top@users.noreply.github.com>
2026-02-21 11:16:11 +0700

    เพิ่มคลาส Money และทดสอบการทำงานใน Lesson 4

--- Lesson_4/domain/TrackingNumber.py
+++ Lesson_4/domain/TrackingNumber.py
@@ -11,6 +11,18 @@ class TrackingNumber(BaseModel):
         return v.strip()
     
 
+class Money(BaseModel):
+
+    amount: float = Field(..., ge=0)
+    currency: str = Field(..., min_length=3, max_length=3)
+
+    @field_validator('currency')
+    def currency_must_be_uppercase(cls, v):
+        return v.upper()
+    
+    def __str__(self):
+        return f'{self.amount:,.2f} {self.currency}'
+
 if __name__ == "__main__":
     good_track = TrackingNumber(value='TH1234567890')
     print(f'สำเร็จ: ได้รับพัสดุ {good_track.value}')
@@ -19,3 +31,11 @@ if __name__ == "__main__":
         bad_track = TrackingNumber(value='TH12')
     except ValueError as e:
         print(f'โดนดัก error {e}')
+
+    try:
+        claim_money = Money(amount=1477.50, currency='thb')
+        print(f'สร้างสำเร็จ: {claim_money}')
+    
+        fake_money = Money(amount=-50, currency='usd')
+    except Exception as e:
+        print(f'ระบบดักจับเงินติดลบได้ {e}')
--- Lesson_4/test_value_objects.py
+++ Lesson_4/test_value_objects.py
@@ -2,7 +2,7 @@ import pytest
 from pydantic import ValidationError
 
 # สมมติว่าเราเรียกใช้คลาสจากไฟล์ domain (ที่เรากำลังจะสร้าง)
-from domain.TrackingNumber import TrackingNumber
+from domain.TrackingNumber import TrackingNumber, Money
 
 # -----------------------------------------
 # Test Cases สำหรับ TrackingNumber
@@ -24,3 +24,22 @@ def test_invalid_type_tracking_number():
     with pytest.raises(ValidationError):
         TrackingNumber(value=12345678) # ต้องเป็น String เท่านั้น
 
+# -----------------------------------------
+# Test Cases สำหรับ Money
+# -----------------------------------------
+
+def test_valid_money_creation():
+    # จำลองค่าจากไฟล์ป๋า: 886.26 THB
+    m = Money(amount=886.26, currency="THB")
+    assert m.amount == 886.26
+    assert m.currency == "THB"
+
+def test_money_negative_amount():
+    # กฎคือเงินเคลมต้องไม่ติดลบ ถ้าติดลบต้องพัง (Error)
+    with pytest.raises(ValidationError):
+        Money(amount=-100.0, currency="THB")
+
+def test_money_invalid_currency_format():
+    # สกุลเงินต้องมี 3 ตัวอักษรเท่านั้น (มาตรฐาน ISO)
+    with pytest.raises(ValidationError):
+        Money(amount=100.0, currency="THAI_BAHT")
